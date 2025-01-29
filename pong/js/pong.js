6756efd6da399e44111f576b687348c17a8edf4c
Arochio <Cooperbgraves@yahoo.com>
2025-01-29 16:16:02 -0500

    Update pong.js

--- pong/js/pong.js
+++ pong/js/pong.js
@@ -60,7 +60,7 @@ function main()
         pad[i].vy *= fy
 
         if(pad[i].y < 0 + (pad[i].h/2) || pad[i].y > 800 - (pad[i].h/2)) {
-            var mult1 = pad[i].y / Math.abs(pad[i].y)
+            var mult1 = (pad[i].y / Math.abs(pad[i].y)) * -1
             pad[i].y = 400 + (400 * mult1) + ((pad[i].h / 2) * (mult1 * -1))
         }
 
